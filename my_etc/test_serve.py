02179c0521c31ac5a45411966f24b0e0fae4ca2c
do-dong-park <rnlgksclsrn9868@gmail.com>
2023-11-30 12:48:40 +0900

    Revert "add feature; save log to db"
    This reverts commit 70a93b0d19f03d4215eae8944b451b8c5168fa00.

--- my_etc/test_serve.py
+++ my_etc/test_serve.py
@@ -1,18 +1,14 @@
 import requests
-import random
-
 BASE_URL = "http://localhost:8080/"
 PRED_URL = "predictions/roberta"
-
 def prediction(payload):
-    response = requests.post(BASE_URL + PRED_URL, json=payload)
-    return response.json()
+	response = requests.post(BASE_URL+PRED_URL, json=payload)
+	return response.json()
 
-adjectives = ["happy", "sad", "excited", "calm", "anxious", "joyful", "angry"]
-input_adj = random.choice(adjectives)
-
-server_status_test_result = requests.get(BASE_URL + "ping").json()
-prediction_test_result = prediction({"inputs": [input_adj]})
+server_status_test_result = requests.get(BASE_URL+"ping").json()
+prediction_test_result = prediction({
+    "inputs" : ["so sleepy", "so dad", "beautiful night"]
+})
 
 print(server_status_test_result)
-print(f"input : {input_adj}, output : {prediction_test_result}")
+print(prediction_test_result)
